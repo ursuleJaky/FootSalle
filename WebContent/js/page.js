07e55e6dd7b3bcb215b0bbd2bb5fca71ff16eecf
Yassine <Yassine@squizo.local>
2018-04-10 15:36:46 +0200

    J'ai réglé les conflits sur bootstrap.js & page.js

--- WebContent/js/page.js
+++ WebContent/js/page.js
@@ -1,4 +1,26 @@
-<<<<<<< HEAD
+$(document).ready(function(){
+    $(".dropdown-toggle").dropdown("toggle");
+});
+
+//nav bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+//map
+function myMap() {
+    var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {center: myCenter, zoom: 5};
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+}
 
 
 $(document).ready(function(){
@@ -24,32 +46,3 @@ function myMap() {
     var marker = new google.maps.Marker({position:myCenter});
     marker.setMap(map);
 }
-
-=======
-
-
-$(document).ready(function(){
-    $(".dropdown-toggle").dropdown("toggle");
-});
-
-//nav bar
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-//map
-function myMap() {
-    var myCenter = new google.maps.LatLng(51.508742,-0.120850);
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {center: myCenter, zoom: 5};
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({position:myCenter});
-    marker.setMap(map);
-}
-
->>>>>>> 3ffd10569fc2d47390d054aa355fffce4815f5a3
